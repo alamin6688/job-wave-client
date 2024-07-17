1e2284d0bb6d65d15c366db691db752d3cc30a7a
alamin6688 <alamin.kzs06@gmail.com>
2024-07-18 02:52:22 +0600

    Navbar & Footer modified

--- src/Pages/Shared/Footer/Footer.jsx
+++ src/Pages/Shared/Footer/Footer.jsx
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (
-    <footer className="footer md:footer-center bg-neutral text-neutral-content">
-      <div className="footer max-w-screen-2xl mx-auto p-10">
+    <footer className="footer md:footer-center bg-neutral text-neutral-content p-10">
+      <div className="footer max-w-screen-2xl mx-auto">
         <nav>
           <h6 className="footer-title">Services</h6>
           <a className="link link-hover">Branding</a>
@@ -22,24 +22,6 @@ const Footer = () => {
           <a className="link link-hover">Privacy policy</a>
           <a className="link link-hover">Cookie policy</a>
         </nav>
-        <form>
-          <h6 className="footer-title">Newsletter</h6>
-          <fieldset className="form-control w-80">
-            <label className="label">
-              <span className="label-text text-neutral-content">
-                Enter your email address
-              </span>
-            </label>
-            <div className="join">
-              <input
-                type="text"
-                placeholder="username@site.com"
-                className="input input-bordered border-none join-item"
-              />
-              <button className="btn btn-primary join-item">Subscribe</button>
-            </div>
-          </fieldset>
-        </form>
       </div>
     </footer>
   );
--- src/Pages/Shared/Navbar/Navbar.jsx
+++ src/Pages/Shared/Navbar/Navbar.jsx
@@ -3,10 +3,8 @@ import { Link, NavLink } from "react-router-dom";
 import useAuth from "../../../Hooks/UseAuth";
 
 const Navbar = () => {
-  //   const { user, logOut } = useAuth();
   const { user, logOut } = useAuth();
   console.log(user);
-  // Check localStorage for theme, default to "light" if not set
   const storedTheme = localStorage.getItem("theme") || "light";
   const [theme, setTheme] = useState(storedTheme);
 
@@ -34,22 +32,18 @@ const Navbar = () => {
   );
 
   useEffect(() => {
-    // Store theme in localStorage
     localStorage.setItem("theme", theme);
-    // Set theme on HTML element
     document.querySelector("html").setAttribute("data-theme", theme);
   }, [theme]);
 
   const handleToggle = () => {
-    // Toggle between light and dark theme
     setTheme((prevTheme) => (prevTheme === "light" ? "dark" : "light"));
   };
 
   return (
-    <div className="shadow-xl">
-      <div className="navbar max-w-screen-xl mx-auto px-4">
+    <div className="shadow-xl z-50 relative">
+      <div className="navbar max-w-screen-2xl mx-auto px-4">
         <div className="navbar-start">
-          {/* Toggle button for mobile */}
           <div className="dropdown">
             <div
               tabIndex={0}
@@ -71,26 +65,21 @@ const Navbar = () => {
                 />
               </svg>
             </div>
-            {/* Dropdown menu for mobile */}
-            <ul className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 shadow font-semibold">
+            <ul className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1000] mt-3 w-52 shadow font-semibold">
               {navLinks}
             </ul>
           </div>
-          {/* Logo */}
           <NavLink
             to="/"
-            className="btn btn-ghost pl-1 gap-0 text-2xl md:text-4xl"
+            className="btn btn-ghost pl-1 gap-0 text-3xl md:text-4xl"
           >
             Job<span className="text-green-500">Wave</span>
           </NavLink>
         </div>
-        {/* Navigation links for desktop */}
         <div className="navbar-center hidden lg:flex">
           <ul className="menu menu-horizontal font-bold px-1">{navLinks}</ul>
         </div>
-        {/* Theme toggle and user avatar */}
-        <div className="navbar-end gap-4">
-          {/* Theme toggle */}
+        <div className="navbar-end gap-0">
           <div className="mt-1">
             <label className="swap swap-rotate">
               <input
@@ -99,7 +88,6 @@ const Navbar = () => {
                 className="theme-controller"
                 checked={theme === "dark"}
               />
-              {/* Sun icon */}
               <svg
                 className={`swap-off h-10 w-10 fill-current ${
                   theme === "dark" ? "opacity-30" : ""
@@ -109,9 +97,8 @@ const Navbar = () => {
               >
                 <path d="M5.64,17l-.71.71a1,1,0,0,0,0,1.41,1,1,0,0,0,1.41,0l.71-.71A1,1,0,0,0,5.64,17ZM5,12a1,1,0,0,0-1-1H3a1,1,0,0,0,0,2H4A1,1,0,0,0,5,12Zm7-7a1,1,0,0,0,1-1V3a1,1,0,0,0-2,0V4A1,1,0,0,0,12,5ZM5.64,7.05a1,1,0,0,0,.7.29,1,1,0,0,0,.71-.29,1,1,0,0,0,0-1.41l-.71-.71A1,1,0,0,0,4.93,6.34Zm12,.29a1,1,0,0,0,.7-.29l.71-.71a1,1,0,1,0-1.41-1.41L17,5.64a1,1,0,0,0,0,1.41A1,1,0,0,0,17.66,7.34ZM21,11H20a1,1,0,0,0,0,2h1a1,1,0,0,0,0-2Zm-9,8a1,1,0,0,0-1,1v1a1,1,0,0,0,2,0V20A1,1,0,0,0,12,19ZM18.36,17A1,1,0,0,0,17,18.36l.71.71a1,1,0,0,0,1.41,0,1,1,0,0,0,0-1.41ZM12,6.5A5.5,5.5,0,1,0,17.5,12,5.51,5.51,0,0,0,12,6.5Zm0,9A3.5,3.5,0,1,1,15.5,12,3.5,3.5,0,0,1,12,15.5Z" />
               </svg>
-              {/* Moon icon */}
               <svg
-                className={`swap-on h-10 w-10 fill-current ${
+                className={`swap-on h-10 w-10 mr-2 fill-current ${
                   theme === "light" ? "opacity-30" : ""
                 }`}
                 xmlns="http://www.w3.org/2000/svg"
@@ -121,52 +108,57 @@ const Navbar = () => {
               </svg>
             </label>
           </div>
-          {/* User avatar */}
-          <div className="dropdown dropdown-end">
-            <div
-              tabIndex={0}
-              role="button"
-              className="btn btn-ghost btn-circle avatar"
-            >
-              <div
-                className="w-10 rounded-full"
-                title={user?.displayName || "Guest"}
+          {!user && (
+            <div>
+              <Link
+                to="/sign-in"
+                className="btn w-full bg-green-500 hover:bg-green-600 text-white border-none hover:font-bold"
               >
-                <img
-                  alt="User avatar"
-                  src={
-                    user?.photoURL ||
-                    "https://i.postimg.cc/wM5cB69C/istockphoto-1300845620-612x612.jpg"
-                  }
-                />
-              </div>
+                Sign In
+              </Link>
             </div>
-            {/* Dropdown menu for user actions */}
-            <ul className="menu menu-sm dropdown-content bg-base-200 rounded-box z-[1] mt-3 w-44 p-2 space-y-1 shadow font-semibold">
-              <li>
-                <a>Profile</a>
-              </li>
-              <li>
-                <a>Settings</a>
-              </li>
-              <li>
-                {user ? (
-                  <Link
-                    onClick={handleLogout}
-                    className="btn bg-red-500 hover:bg-red-600 text-white border-none hover:font-bold"
+          )}
+          <div className="dropdown dropdown-end">
+            {user && (
+              <div>
+                <div
+                  tabIndex={0}
+                  role="button"
+                  className="btn btn-ghost btn-circle avatar"
+                >
+                  <div
+                    className="w-10 rounded-full"
+                    title={user?.displayName || "Guest"}
                   >
-                    Sign Out
-                  </Link>
-                ) : (
-                  <Link
-                    to="/sign-in"
-                    className="btn w-full bg-green-500 hover:bg-green-600 text-white border-none hover:font-bold"
-                  >
-                    Sign In{" "}
-                  </Link>
-                )}
-              </li>
-            </ul>
+                    <img
+                      alt="User avatar"
+                      src={
+                        user?.photoURL ||
+                        "https://i.postimg.cc/wM5cB69C/istockphoto-1300845620-612x612.jpg"
+                      }
+                    />
+                  </div>
+                </div>
+                <ul className="menu menu-sm dropdown-content bg-base-200 rounded-box z-[1000] mt-3 w-44 p-2 space-y-1 shadow font-semibold">
+                  <li>
+                    <a>Profile</a>
+                  </li>
+                  <li>
+                    <a>Blogs</a>
+                  </li>
+                  <li>
+                    {user && (
+                      <Link
+                        onClick={handleLogout}
+                        className="btn bg-red-500 hover:bg-red-600 text-white border-none hover:font-bold"
+                      >
+                        Sign Out
+                      </Link>
+                    )}
+                  </li>
+                </ul>
+              </div>
+            )}
           </div>
         </div>
       </div>
