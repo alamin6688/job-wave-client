33271e2d51abe59f0fb4952448f2afd285d65630
alamin6688 <alamin.kzs06@gmail.com>
2024-08-22 22:37:55 +0600

    Sign In Page Design Modified

--- src/Pages/SignIn/SignIn.jsx
+++ src/Pages/SignIn/SignIn.jsx
@@ -35,67 +35,98 @@ const SignIn = () => {
       <Helmet>
         <title>Job Wave | Sign In</title>
       </Helmet>
-      <div className="min-h-[calc(100vh-285px)] flex items-center justify-center bg-base-200 py-10 px-4 md:py-16">
-        <div className="hero-content w-full">
-          <div className="card w-full max-w-sm shadow-2xl bg-base-100">
-            <div className="text-center pt-6">
-              <h3 className="text-4xl font-bold">Sign In</h3>
+      <div className="flex justify-center items-center min-h-[calc(100vh-286px)]">
+        <div className="flex w-full max-w-sm mx-auto overflow-hidden bg-white rounded-xl shadow-xl  lg:max-w-4xl ">
+          <div
+            className="hidden bg-cover bg-center lg:block lg:w-1/2"
+            style={{
+              backgroundImage: `url('https://images.unsplash.com/photo-1606660265514-358ebbadc80d?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1575&q=80')`,
+            }}
+          ></div>
+
+          <div className="w-full px-6 py-8 md:px-8 lg:w-1/2">
+            <div className="flex justify-center mx-auto">
+              <img
+                className="w-auto h-7 sm:h-8"
+                src="https://merakiui.com/images/logo.svg"
+                alt=""
+              />
             </div>
-            <form onSubmit={handleLogin} className="card-body">
-              <div className="form-control">
-                <label className="label">
-                  <span className="label-text font-bold">Email</span>
-                </label>
-                <input
-                  type="email"
-                  name="email"
-                  placeholder="email"
-                  className="input input-bordered"
-                  required
-                />
-              </div>
-              <div className="form-control">
-                <label className="label">
-                  <span className="label-text font-bold">Password</span>
-                </label>
-                <input
-                  type="password"
-                  name="password"
-                  placeholder="password"
-                  className="input input-bordered"
-                  required
-                />
-                <label className="label">
-                  <a
-                    href="#"
-                    className="label-text-alt font-semibold link link-hover"
-                  >
-                    Forgot password?
-                  </a>
-                </label>
+
+            <p className="mt-3 text-xl text-center text-gray-600 ">
+              Welcome back!
+            </p>
+
+            <div>
+              <SocialLogin></SocialLogin>
+            </div>
+
+            <div className="flex items-center justify-between mt-4">
+              <span className="w-1/5 border-b  lg:w-1/4"></span>
+
+              <div className="text-xs text-center text-gray-500 uppercase  hover:underline">
+                or login with email
               </div>
 
-              <div className="form-control mt-6">
+              <span className="w-1/5 border-b dark:border-gray-200 lg:w-1/4"></span>
+            </div>
+            <form onSubmit={handleLogin}>
+              <div className="mt-4">
+                <label
+                  className="block mb-2 text-sm font-medium text-gray-600 "
+                  htmlFor="LoggingEmailAddress"
+                >
+                  Email Address
+                </label>
                 <input
-                  className="btn btn-ghost bg-green-500 hover:bg-green-600 text-xl font-bold text-white"
-                  type="submit"
-                  value="Sign In"
+                  id="LoggingEmailAddress"
+                  autoComplete="email"
+                  name="email"
+                  className="block w-full px-4 py-2 text-gray-700 bg-white border rounded-lg    focus:border-blue-400 focus:ring-opacity-40  focus:outline-none focus:ring focus:ring-blue-300"
+                  type="email"
                 />
               </div>
-              <div className="text-center mt-4">
-                <p className="font-bold text-red-400">
-                  <small>
-                    Don&apos;t have any account?{" "}
-                    <Link to="/sign-up">
-                      <span className="underline text-red-600">
-                        Sign Up Now
-                      </span>
-                    </Link>{" "}
-                  </small>
-                </p>
+
+              <div className="mt-4">
+                <div className="flex justify-between">
+                  <label
+                    className="block mb-2 text-sm font-medium text-gray-600 "
+                    htmlFor="loggingPassword"
+                  >
+                    Password
+                  </label>
+                </div>
+
+                <input
+                  id="loggingPassword"
+                  autoComplete="current-password"
+                  name="password"
+                  className="block w-full px-4 py-2 text-gray-700 bg-white border rounded-lg    focus:border-blue-400 focus:ring-opacity-40  focus:outline-none focus:ring focus:ring-blue-300"
+                  type="password"
+                />
               </div>
-              <SocialLogin></SocialLogin>
+              <div className="mt-6">
+                <button
+                  type="submit"
+                  className="w-full px-6 py-3 text-sm font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-gray-800 rounded-lg hover:bg-gray-700 focus:outline-none focus:ring focus:ring-gray-300 focus:ring-opacity-50"
+                >
+                  Sign In
+                </button>
+              </div>
             </form>
+
+            <div className="flex items-center justify-between mt-4">
+              <span className="w-1/5 border-b  md:w-1/4"></span>
+
+              <Link
+                to="/sign-up"
+                className="text-xs font-bold text-gray-600 uppercase  hover:underline"
+              >
+                or sign up
+              </Link>
+
+              <span className="w-1/5 border-b  md:w-1/4"></span>
+            </div>
           </div>
         </div>
       </div>
