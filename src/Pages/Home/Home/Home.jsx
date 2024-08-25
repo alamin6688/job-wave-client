e99a1d343de0ffc09996f4699e6c5744eef58cb5
alamin6688 <alamin.kzs06@gmail.com>
2024-08-25 22:11:44 +0600

    JSON Data Added

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -1,15 +1,13 @@
-import { useLoaderData } from "react-router-dom";
 import Banner from "../Banner/Banner";
 import TabCategories from "../TabCategories/TabCategories";
 
 
 const Home = () => {
-    const jobs = useLoaderData()
-    console.log(jobs);
+
     return (
         <div className="min-h-[calc(100vh-286px)]">
             <Banner></Banner>
-            <TabCategories jobs={jobs}></TabCategories>
+            <TabCategories></TabCategories>
         </div>
     );
 };
--- src/Pages/Home/TabCategories/TabCategories.jsx
+++ src/Pages/Home/TabCategories/TabCategories.jsx
@@ -1,15 +1,35 @@
 import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
 import "react-tabs/style/react-tabs.css";
 import JobCards from "./JobCards";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
-const TabCategories = ({ jobs }) => {
+const TabCategories = () => {
+  const [jobs, setJobs] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(`${import.meta.env.VITE_API_URL}/jobs`)
+      .then((res) => {
+        setJobs(res.data);
+      })
+      .catch((error) => {
+        console.error("Error fetching jobs:", error);
+      });
+  }, []); // Empty dependency array means this effect runs once on mount
+
+  // Utility function to filter jobs based on category
+  const filterJobsByCategory = (category) => {
+    return jobs.filter((job) => job.category === category);
+  };
+
   return (
     <div className="max-w-screen-2xl mx-auto mt-4 mb-12 pb-8 px-4">
       <h1 className="text-2xl font-bold text-center capitalize lg:text-3xl pt-4">
         Browse Jobs By Categories
       </h1>
 
-      <p className="w-full md:w-3/4 mx-auto my-6 text-center text-gray-500 ">
+      <p className="w-full md:w-3/4 mx-auto my-6 text-center text-gray-500">
         Explore a wide range of job opportunities tailored to your skills and
         interests. Start browsing today and find your next big project! Connect
         with top professionals and elevate your career.
@@ -27,62 +47,54 @@ const TabCategories = ({ jobs }) => {
         </div>
         <TabPanel>
           <div className="grid grid-cols-1 gap-8 mt-8 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-            {jobs
-              // .filter((j) => j.category === "Web Development")
-              .map((job) => (
-                <JobCards key={job._id} job={job} />
-              ))}
+            {filterJobsByCategory("Web Development").map((job) => (
+              <JobCards key={job._id} job={job} />
+            ))}
           </div>
         </TabPanel>
 
         <TabPanel>
           <div className="grid grid-cols-1 gap-8 mt-8 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-            {jobs
-              // .filter((j) => j.category === "Graphics Design")
-              .map((job) => (
-                <JobCards key={job._id} job={job} />
-              ))}
+            {filterJobsByCategory("Graphics Design").map((job) => (
+              <JobCards key={job._id} job={job} />
+            ))}
           </div>
         </TabPanel>
 
         <TabPanel>
           <div className="grid grid-cols-1 gap-8 mt-8 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-            {jobs
-              // .filter((j) => j.category === "Digital Marketing")
-              .map((job) => (
-                <JobCards key={job._id} job={job} />
-              ))}
+            {filterJobsByCategory("Digital Marketing").map((job) => (
+              <JobCards key={job._id} job={job} />
+            ))}
           </div>
         </TabPanel>
+
         <TabPanel>
           <div className="grid grid-cols-1 gap-8 mt-8 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-            {jobs
-              // .filter((j) => j.category === "Digital Marketing")
-              .map((job) => (
-                <JobCards key={job._id} job={job} />
-              ))}
+            {filterJobsByCategory("Data Analyst").map((job) => (
+              <JobCards key={job._id} job={job} />
+            ))}
           </div>
         </TabPanel>
+
         <TabPanel>
           <div className="grid grid-cols-1 gap-8 mt-8 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-            {jobs
-              // .filter((j) => j.category === "UI/UX Design")
-              .map((job) => (
-                <JobCards key={job._id} job={job} />
-              ))}
+            {filterJobsByCategory("Content Writer").map((job) => (
+              <JobCards key={job._id} job={job} />
+            ))}
           </div>
         </TabPanel>
+
         <TabPanel>
           <div className="grid grid-cols-1 gap-8 mt-8 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-            {jobs
-              // .filter((j) => j.category === "UI/UX Design")
-              .map((job) => (
-                <JobCards key={job._id} job={job} />
-              ))}
+            {filterJobsByCategory("UI/UX Design").map((job) => (
+              <JobCards key={job._id} job={job} />
+            ))}
           </div>
         </TabPanel>
       </Tabs>
     </div>
   );
 };
+
 export default TabCategories;
