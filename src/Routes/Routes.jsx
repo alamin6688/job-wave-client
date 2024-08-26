80e9843abc677cff3febd5b7b107530487a4ff7d
alamin6688 <alamin.kzs06@gmail.com>
2024-08-26 23:56:53 +0600

    Add Job Page Added

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -4,6 +4,7 @@ import Home from "../Pages/Home/Home/Home";
 import SignUp from "../Pages/SignUp/SignUp";
 import SignIn from "../Pages/SignIn/SignIn";
 import JobDetails from "../Pages/JobDetails/JobDetails";
+import AddJob from "../Pages/AddJob/AddJob";
 
 export const router = createBrowserRouter([
   {
@@ -21,6 +22,10 @@ export const router = createBrowserRouter([
       {
         path: "/sign-in",
         element: <SignIn></SignIn>,
+      },
+      {
+        path: "/add-job",
+        element: <AddJob></AddJob>,
       },
       {
         path: "/job/:id",
