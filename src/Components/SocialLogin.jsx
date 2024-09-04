08781dd5dd196a7736531b3f6b9c648f9facfa05
alamin6688 <alamin.kzs06@gmail.com>
2024-09-04 23:09:03 +0600

    Modified

--- src/Components/SocialLogin.jsx
+++ src/Components/SocialLogin.jsx
@@ -26,7 +26,7 @@ const SocialLogin = () => {
       };
       console.log(userInfo);
 
-      // 3. Get token from server using email
+      // 3. Get Token From Server Using Email
       const { data } = await axios.post(
         `${import.meta.env.VITE_API_URL}/jwt`,
         { email: result?.user?.email },
